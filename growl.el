11310d5684c3251781c6e968881df886d6ac0df4
Edward O'Connor <ted@evdb.com>
2010-11-30 13:43:04 +0100

    update

--- growl.el
+++ growl.el
@@ -4,6 +4,7 @@
 
 ;; Author: Edward O'Connor <ted@evdb.com>
 ;; Keywords: convenience
+;; Version: 1.0.0
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
